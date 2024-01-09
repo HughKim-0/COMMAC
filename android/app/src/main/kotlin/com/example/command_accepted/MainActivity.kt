2f084c5cf9a3f549b71d3750d964316826c432e1
Hugh Kim <wooni05@gmail.com>
2024-01-09 17:04:56 -0500

    second_commit

---
+++ android/app/src/main/kotlin/com/example/command_accepted/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.command_accepted
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/command_order_alert/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.command_order_alert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
